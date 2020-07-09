a16c07ef68ae2b0d55afc35aa4177274d280c1f5
andreas schlegel <andi@sojamo.de>
2020-07-09 15:54:48 +0800

    Add NetAddress class to oscp5 package then noetp5 package doesn't need to be  added to the claspath

--- src/main/java/oscP5/NetAddress.java
+++ src/main/java/oscP5/NetAddress.java
@@ -1,30 +1,57 @@
 package oscP5;
 
-import java.net.InetAddress;
-
 import netP5.ANetAddress;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
+/**
+ * Holds the network address and port number a network packet should be sent to.
+ */
 public class NetAddress extends ANetAddress {
 
-    public NetAddress(final int thePort)
-    {
-        super(LOCALHOST_ADDR, thePort);
+    static public String DEFAULT_ADDR = "0.0.0.0";
+    static public final String LOCALHOST_ADDR = "127.0.0.1";
+    private final InetSocketAddress socketAddress;
+
+    public NetAddress(final int thePort) {
+        this(LOCALHOST_ADDR, thePort);
     }
 
     public NetAddress(final String theAddress,
-            final int thePort)
-    {
-        super(theAddress, thePort);
+                      final int thePort) {
+        super();
+        socketAddress = new InetSocketAddress(theAddress, thePort);
     }
 
-    public NetAddress(final NetAddress theNetAddress)
-    {
-        super(theNetAddress.getAddress(), theNetAddress.getPort());
+    public NetAddress(final NetAddress theNetAddress) {
+        this(theNetAddress.getAddress(), theNetAddress.getPort());
     }
 
     public NetAddress(final InetAddress theInetAddress,
-            final int thePort)
-    {
-        super(theInetAddress, thePort);
+                      final int thePort) {
+        super();
+        socketAddress = new InetSocketAddress(theInetAddress, thePort);
+    }
+
+    @Override
+    public InetAddress getInetAddress() {
+        return socketAddress.getAddress();
+    }
+
+    @Override
+    public String getAddress() {
+        return socketAddress.getAddress().getHostAddress();
+    }
+
+    @Override
+    public int getPort() {
+        return socketAddress.getPort();
+    }
+
+    public String toString() {
+        return "{class: NetAddress"+
+                ", address: "+getAddress() +
+                ", port: "+ getPort() +
+                "}";
     }
 }
